9de714de0be4a31a9c385084b5a788ba8f7a1e04
SamuelOnah <samuelonah81@gmail.com>
2026-07-15 16:56:06 +0100

    day1

---
+++ Day1.py
@@ -0,0 +1,17 @@
+
+# Comment
+# Declaring a variable
+x = 10
+y = 20
+o = "Python"
+n = "is"
+a = "fun"
+h = "actually"
+# printing out expected output
+
+print(x)
+print(y)
+print(o,n,a,h)
+print("onah samuel")
+
+
