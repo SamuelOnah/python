4eae56eab3d9b697358bc0e907f5aaa774aa9dd2
sonah <samuelonah81@gmail.com>
2026-07-16 15:52:55 +0100

    loop

---
+++ loop/count.py
@@ -0,0 +1,12 @@
+for count in range(1, 6):
+    print(f" count: {count}")
+    # print("done")  this line prints done after each line 
+
+
+colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    print(f"My favourite color is {color}")
+
+for i in range(1,6):
+    print("*" *i)
+
