457fb82f0c1497a3cd076ff35f91750e5cbff9a6
sonah <samuelonah81@gmail.com>
2026-07-16 11:28:09 +0100

    Day2

--- Day1.py
+++ Day1.py
@@ -14,4 +14,22 @@ print(y)
 print(o,n,a,h)
 print("onah samuel")
 
+name = "Onah Samuel"
+age = 20 
+height = 5.9
+is_student = True 
+a = 10
+b = 3
 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a+b)
+print(a/b)
+print(a+b)
+print(a**b)
+print("My name is", name)
+print("I am", age, "years old")
+print("Height:", height)
+print("Am I a student?", is_student)
