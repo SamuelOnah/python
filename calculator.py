fcaa1127b11c3d97d5fe18a7339d94c99109a8b1
sonah <samuelonah81@gmail.com>
2026-07-16 11:49:02 +0100

    calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+print("=== SIMPLE CALCULATOR ===")
+
+
+
+num1  = float(input("Enter First number: ")) # float() handler decimals
+num2 = float(input("Enter second number: "))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+divisions = num1 / num2
+
+
+print("\n---RESULT---")
+print(num1, "+", num2, "=", sum_result)
+print(num1, "-",  num2, "=", difference)
+print(num1, "*", num2, "=", product)
+print(num1,"/", num2, "=", divisions)
+
