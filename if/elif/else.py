28692be15dca4e5e1fd719062c66f4f39877986e
sonah <samuelonah81@gmail.com>
2026-07-16 12:01:37 +0100

    TINUBU if

---
+++ if/elif/else.py
@@ -0,0 +1,15 @@
+# Basic if statement 
+
+age = int(input("How old are you? "))
+
+
+if age >= 18:
+    print("you're an adult!")
+    print("you can vote")
+
+else:
+        print("You're a minor")
+        print("keep growing!")
+
+print("Thanks for telling me your age")
+print("Make sure you dont vote TINUBU")
