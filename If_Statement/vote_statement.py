32b777543e7334acd9e921287f2056bdbeaf9495
sonah <samuelonah81@gmail.com>
2026-07-16 15:09:57 +0100

    if

--- If_Statement/vote_statement.py
+++ If_Statement/vote_statement.py
@@ -2,14 +2,11 @@
 
 age = int(input("How old are you? "))
 
-
 if age >= 18:
     print("you're an adult!")
     print("you can vote")
-
 else:
         print("You're a minor")
         print("keep growing!")
-
 print("Thanks for telling me your age")
 print("Make sure you dont vote TINUBU")
