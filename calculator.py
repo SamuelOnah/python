140eeaec384222bdd89fcfd92ab82f48ae9563fa
sonah <samuelonah81@gmail.com>
2026-07-16 11:51:43 +0100

    calculatoor

--- calculator.py
+++ calculator.py
@@ -4,6 +4,8 @@ print("=== SIMPLE CALCULATOR ===")
 
 num1  = float(input("Enter First number: ")) # float() handler decimals
 num2 = float(input("Enter second number: "))
+num1  = int(input("Enter First number: ")) # float() handler decimals
+num2 = int(input("Enter second number: "))
 
 sum_result = num1 + num2
 difference = num1 - num2
