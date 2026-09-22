fd018a67fbfe2746fb7a03709f6623fa0e7c790a
sonah <samuelonah81@gmail.com>
2026-09-22 14:08:35 +0100

    passing argurments to a function

---
+++ Day11/arg.py
@@ -0,0 +1,5 @@
+def describe_pet(animal_type, pet_name):
+    """ Display information about a pet """
+    print(f"\n I have a {animal_type}.")
+    print(f"My {animal_type}'s nmae is {pet_name}")
+describe_pet('hamster', 'harry')
