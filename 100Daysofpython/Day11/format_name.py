02843bb2fd7065dcbf7702f1d991c7b92f8aedaf
sonah <samuelonah81@gmail.com>
2026-09-22 16:21:20 +0100

    formatted names

--- 100Daysofpython/Day11/format_name.py
+++ 100Daysofpython/Day11/format_name.py
@@ -1,6 +1,6 @@
-def get_formatted_name(first_name, last_name):
+def get_formatted_name(first_name, middle_name, last_name):
     """Return a full name, neatly formatted"""
-    full_name = f"{first_name} {last_name}"
+    full_name = f"{first_name} {middle_name} {last_name}"
     return full_name.title()
-musician = get_formatted_name('Onah', 'Samuel')
+musician = get_formatted_name('Onah', 'iyojeni', 'samuel')
 print(musician)
