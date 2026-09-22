2625c721823426c131f66243818c6a99c23aca35
sonah <samuelonah81@gmail.com>
2026-09-22 16:43:49 +0100

    Build_Person.

--- 100Daysofpython/Day11/person.py
+++ 100Daysofpython/Day11/person.py
@@ -1,5 +1,7 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age):
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
     return person
-musician = build_person('Iyojeni', 'Samuel')
+musician = build_person('Iyojeni', 'Samuel', age=20)
 print(musician)
