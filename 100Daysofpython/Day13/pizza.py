411df08ad1b45ba368235107f20e406bfe5c059a
sonah <samuelonah81@gmail.com>
2026-09-24 15:30:08 +0100

    pizza

---
+++ 100Daysofpython/Day13/pizza.py
@@ -0,0 +1,10 @@
+def make_pizza(*toopings):
+    print(toopings)
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheeese')
